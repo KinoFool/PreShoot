d73aee38d94df25b9dc2d8d2b70605c2566f973e
José Jonathan Emmanuel <jonthan-emmanuel.jose@epitech.eu>
2020-05-31 09:15:01 +0200

    Add musics on menu

--- Scripts/Menu/ExitButton.gd
+++ Scripts/Menu/ExitButton.gd
@@ -1,4 +1,12 @@
 extends TextureButton
 
+onready var back = get_parent().get_node("Node/Back")
+onready var menu = get_parent().get_node("MusicMenu")
+
 func _on_ExitButton_pressed():
-	get_tree().quit()
+	menu.stop()
+	back.play()
+
+func _physics_process(delta):
+	if back.get_playback_position() > 1:
+		get_tree().quit()
--- Scripts/Menu/PlayButton.gd
+++ Scripts/Menu/PlayButton.gd
@@ -1,5 +1,8 @@
 extends TextureButton
 
+onready var select = get_parent().get_node("Node/Select")
+
 func _on_PlayButton_pressed():
+	select.play()
 	print("CLICK")
 	get_tree().change_scene("res://Scenes/Maps/Level 1.tscn")
