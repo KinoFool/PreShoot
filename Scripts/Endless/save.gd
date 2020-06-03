3f0b9951f44181a839bee1731a26df2721637c14
José Jonathan Emmanuel <jonthan-emmanuel.jose@epitech.eu>
2020-06-03 11:36:01 +0200

    Starting save handle

---
+++ Scripts/Endless/save.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var dispVal = 0
+
+var path = "res://save.ps"
+var config = ConfigFile.new()
+var load_response = config.load(path)
+
+func _ready():
+	pass
+
+func saveValue():
+	pass
+	
+func loadValue():
+	pass
