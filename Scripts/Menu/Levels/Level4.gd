7df11b23fb4b0a0c371c74bc512383de0b46f3e7
KinoFool <nicolas.mary@epitech.eu>
2020-06-03 18:11:37 +0200

    Rework of level 1 to 6

--- Scripts/Menu/Levels/Level4.gd
+++ Scripts/Menu/Levels/Level4.gd
@@ -3,4 +3,4 @@ extends TextureButton
 var unlocked = true
 
 func _on_Level4_pressed():
-	if unlocked: get_tree().change_scene("res://Scenes/Maps/level 4.tscn")
+	if unlocked: get_tree().change_scene("res://Scenes/Maps/Level 4.tscn")
