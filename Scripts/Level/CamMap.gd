7f576fe894346f5bd4585ae5a82c72d9a0fdaa1a
KinoFool <nicolas.mary@epitech.eu>
2020-06-03 19:33:20 +0200

    Repair cam map

--- Scripts/Level/CamMap.gd
+++ Scripts/Level/CamMap.gd
@@ -1,2 +1,16 @@
 extends Camera2D
 
+var cam_motion = Vector2()
+var cam_pos = self.position.x
+var right_limit = get_limit(MARGIN_RIGHT)
+
+var speed = 10
+
+func _process(delta):
+	cam_pos = self.position.x
+	print (right_limit)
+	print (self.position.x - 1920)
+	if Input.is_action_pressed("ui_left_cam") and cam_pos >= speed:
+		self.position.x -= speed
+	if Input.is_action_pressed("ui_right_cam") and (self.position.x + 1920) < right_limit:
+		self.position.x += speed
